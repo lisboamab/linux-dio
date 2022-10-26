3887b751a2afc1bf1290a713189716052e71abe4
lisboamab <lisboamab@gmail.com>
2022-10-26 12:02:54 -0300

    Conclui adição de  usuarios e adiciona comentarios

--- configVM.sh
+++ configVM.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+#Cria os diretorios e os grupos
 mkdir /publico /adm /ven /sec
 groupadd GRP_ADM GRP_VEN GRP_SEC
 
+#Modifica o grupo dos diretorios
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 
+#Modifica permissões
 chmod 770 /adm /ven /sec
 chmod 777 /publico
 
@@ -20,4 +24,9 @@ useradd maria -c "Maria das Graças" -m -s /bin/bash -G GRP_ADM -p $(openssl pas
 useradd debora -c "Debora Nobre" -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -crypt Senha123) -e
 useradd sebastiana -c "Sebastiana França" -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -crypt Senha123) -e
 useradd roberto -c "Roberto Silva" -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -crypt Senha123) -e
+
+#SEC
+useradd josefina -c "Josefina Junyper" -m -s /bin/bash -G GRP_SEC -p $(openssl passwd -crypt Senha123) -e
+useradd amanda -c "Amanda Amelia" -m -s /bin/bash -G GRP_SEC -p $(openssl passwd -crypt Senha123) -e
+useradd rogerio -c "Rogerio Ramedo" -m -s /bin/bash -G GRP_SEC -p $(openssl passwd -crypt Senha123) -e
 
