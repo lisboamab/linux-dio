8d7d5ddbf138fc1c3c73b159bd93fd54f8b74108
Marcos Lisboa <lisboamab@gmail.com>
2022-10-26 10:56:03 -0300

    Adiciona a criação de grupos e permissões

--- configVM.sh
+++ configVM.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 mkdir /publico /adm /ven /sec
+groupadd GRP_ADM GRP_VEN GRP_SEC
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm /ven /sec
+chmod 777 /publico
+
 
