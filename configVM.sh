e1cdbef434f11b1c0b893562b2e39a6edba24c11
lisboamab <lisboamab@gmail.com>
2022-10-26 11:54:12 -0300

    Adiciona a criação de usuarios

--- configVM.sh
+++ configVM.sh
@@ -9,4 +9,15 @@ chown root:GRP_SEC /sec
 chmod 770 /adm /ven /sec
 chmod 777 /publico
 
+#Adiciona os usuarios
+
+#ADM
+useradd carlos -c "Carlos Batista" -m -s /bin/bash -G GRP_ADM -p $(openssl passwd -crypt Senha123) -e
+useradd joao -c "João Menezes" -m -s /bin/bash -G GRP_ADM -p $(openssl passwd -crypt Senha123) -e
+useradd maria -c "Maria das Graças" -m -s /bin/bash -G GRP_ADM -p $(openssl passwd -crypt Senha123) -e
+
+#Vendas
+useradd debora -c "Debora Nobre" -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -crypt Senha123) -e
+useradd sebastiana -c "Sebastiana França" -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -crypt Senha123) -e
+useradd roberto -c "Roberto Silva" -m -s /bin/bash -G GRP_VEN -p $(openssl passwd -crypt Senha123) -e
 
